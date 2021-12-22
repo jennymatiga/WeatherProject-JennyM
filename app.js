0169d5c514b6a673a5eac8ad44c1499980562a89
jennymatiga <96401687+jennymatiga@users.noreply.github.com>
2021-12-22 15:59:50 -0500

    added weather forecast for 5 days

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 // -- START --let variables //
-
-//dating if statement//
-
 let currentDate = new Date();
 let timeAndDay = document.querySelector(".timeAndDay");
 let greetings = document.querySelector(".greetings");
@@ -25,7 +22,6 @@ let nameMonths = [
   "November",
   "December",
 ];
-
 let nameWeeks = [
   "Sunday",
   "Monday",
@@ -35,7 +31,6 @@ let nameWeeks = [
   "Friday",
   "Saturday",
 ];
-
 let textCurrentDate = document.querySelector(".current-date");
 textCurrentDate.innerHTML = `${nameMonths[months]} ${Day}, ${year},`;
 // -- END --let variables //
@@ -48,7 +43,7 @@ if (TimeHour === 0) {
     minute = `0${minute}`;
   } //minute if statement--END//
   greetings.innerHTML = `Hey, Early bird! its`;
-  timeAndDay.innerHTML = `<strong style="font-weight: 700;"> 12: ${minute} am </strong>here.`;
+  timeAndDay.innerHTML = `<strong style="font-weight: 700;"> 12: ${minute} am </strong>,`;
 }
 //***** IF ELSE STATEMENT 2 ******//
 else if (TimeHour < 12) {
@@ -57,7 +52,7 @@ else if (TimeHour < 12) {
     minute = `0${minute}`;
   } //minute if statement--END//
   greetings.innerHTML = `Good Morning! Today is`;
-  timeAndDay.innerHTML = `${nameWeeks[Weeks]},<strong style="font-weight: 700;"> ${TimeHour}: ${minute} am </strong>here.`;
+  timeAndDay.innerHTML = `${nameWeeks[Weeks]},<strong style="font-weight: 700;"> ${TimeHour}: ${minute} am </strong>,`;
 }
 //***** IF ELSE STATEMENT 3 ******//
 else if (TimeHour > 17) {
@@ -65,14 +60,13 @@ else if (TimeHour > 17) {
   if (minute < 10) {
     minute = `0${minute}`;
   }
-
   //minute if statement--END//
   greetings.innerHTML = `Good Evening! Today is`;
   timeAndDay.innerHTML = `${
     nameWeeks[Weeks]
   }, <strong style="font-weight: 700;">${
     TimeHour - 12
-  }: ${minute} pm </strong>here.`;
+  }: ${minute} pm </strong>,`;
 }
 //***** IF ELSE STATEMENT 4 ******//
 else if (TimeHour === 12) {
@@ -81,7 +75,7 @@ else if (TimeHour === 12) {
     minute = `0${minute}`;
   } //minute if statement--END//
   greetings.innerHTML = `Hello There! Today is`;
-  timeAndDay.innerHTML = `${nameWeeks[Weeks]}, <strong style="font-weight: 700;">${TimeHour}: ${minute} pm </strong>here.`;
+  timeAndDay.innerHTML = `${nameWeeks[Weeks]}, <strong style="font-weight: 700;">${TimeHour}: ${minute} pm </strong>,`;
 }
 //***** IF ELSE STATEMENT 5 ******//
 else {
@@ -94,9 +88,125 @@ else {
     nameWeeks[Weeks]
   }, <strong style="font-weight: 700;">${
     TimeHour - 12
-  }: ${minute} pm </strong>here.`;
+  }: ${minute} pm </strong>,`;
 }
 //----END--- of if statement for time//
+
+//--START- FORECAST FUNNCTION FOR SPECIFIC COUNTRY//
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let foreCastHTML = `<div class="row justify-content-center weather-forecast" >`;
+
+  forecast.forEach(function (fore, index) {
+    if (index < 5) {
+      let NewIcon = fore.weather[0].icon;
+
+      // ////////////////////////////////////////
+
+      if (NewIcon == "03d") {
+        NewIcon = `<img src="images/03d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "01d") {
+        NewIcon = `<img src="images/01d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "02d") {
+        NewIcon = `<img src="images/02d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "04d") {
+        NewIcon = `<img src="images/04d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "09d") {
+        NewIcon = `<img src="images/09d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "10d") {
+        NewIcon = `<img src="images/10d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "11d") {
+        NewIcon = `<img src="images/11d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "13d") {
+        NewIcon = `<img src="images/13d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "50d") {
+        NewIcon = `<img src="images/50d.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "01n") {
+        NewIcon = `<img src="images/01n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "02n") {
+        NewIcon = `<img src="images/02n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "03n") {
+        NewIcon = `<img src="images/03n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "04n") {
+        NewIcon = `<img src="images/04n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "09n") {
+        NewIcon = `<img src="images/09n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "10n") {
+        NewIcon = `<img src="images/10n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "11n") {
+        NewIcon = `<img src="images/11n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "13n") {
+        NewIcon = `<img src="images/13n.png" style="width: 50pt;"/>`;
+      } else if (NewIcon === "50n") {
+        NewIcon = `<img src="images/50n.png" style="width: 50pt;"/>`;
+      }
+
+      function formatDay(timestamp) {
+        let date = new Date(timestamp * 1000);
+        let day = date.getDay();
+        let days = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ];
+
+        return days[day];
+      }
+      /////////////////////////////
+      let theNewDt = fore.dt;
+      let alldate = new Date(theNewDt * 1000);
+      let nameMonths = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let months = alldate.getMonth();
+      let Day = alldate.getDate();
+      let year = alldate.getFullYear();
+
+      ///////////////////////////////
+
+      ///////////////////////////
+
+      ////////////////////////////
+      foreCastHTML =
+        foreCastHTML +
+        `<div class="col-lg-2 col-md-4 col-sm-4"  style="padding: 10px">${NewIcon}<div class="sub-weather-degrees">Max:
+        ${Math.ceil(fore.temp.max)}°C | Min: ${Math.ceil(fore.temp.min)}°C</div>
+              <div class="sub-days" id="date1">${
+                nameMonths[months]
+              }, ${Day}, ${year}</div>
+              <div class="sub-weekdays"id="week1">${formatDay(fore.dt)}</div>
+            </div>`;
+    }
+  });
+
+  foreCastHTML = foreCastHTML + `</div>`;
+  forecastElement.innerHTML = foreCastHTML;
+}
+
+///////////////////////
+function getForecast(coordinates) {
+  let apiKey = "dcd07d1aa5fa314f984ce1fb0e330deb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+//--END- FORECAST FUNNCTION FOR SPECIFIC COUNTRY//
 
 // --- START -- Function on Search field//
 function search(pop) {
@@ -107,13 +217,9 @@ function search(pop) {
     moreStartWords.innerHTML = `and in `;
     let moreLastWords = document.querySelector(".moreLastWords");
     moreLastWords.innerHTML = ` it is `;
-
-    //END--date-time - in each countries//
     let myCountryNewName = value.data.sys.country;
-
     let newCountry = document.querySelector("#MyNewCountry");
     newCountry.innerHTML = `${myCountryNewName}, `;
-
     let NewIcon = `${value.data.weather[0].icon}`;
     let OldIcon = document.querySelector("#myicon");
     let newTemp = `${value.data.main.temp}`;
@@ -122,31 +228,25 @@ function search(pop) {
     let humid = document.querySelector("#humid");
     let currentDegrees = document.querySelector(".current-degrees");
     currentDegrees.innerHTML = `${newTemp} °C`;
-
     humid.innerHTML = `with a humidity of ${newHumidity}%.`;
     let weatherIconSay = document.querySelector(".weatherIconSay");
 
     //START -- if statement icon //
-    /////////////////CELCIUS TO FAR/////
-
     function Celcius() {
       let currentDegrees = document.querySelector(".current-degrees");
       currentDegrees.innerHTML = `${Math.ceil(newTemp)} °C`;
     }
-
     function Far() {
       let currentDegrees = document.querySelector(".current-degrees");
       currentDegrees.innerHTML = `${Math.ceil((newTemp * 9) / 5 + 32)} °F`;
     }
-
     let c = document.querySelector("#c");
     let f = document.querySelector("#f");
     c.addEventListener("click", Celcius);
     f.addEventListener("click", Far);
-
     if (NewIcon == "03d") {
       OldIcon.innerHTML = `<img src="images/03d.png" style="width: 300pt;"/>`;
-      weatherIconSay.innerHTML = `<div style="color: #B5E1E0;">It has "scattered clouds" in the day.</div>`;
+      weatherIconSay.innerHTML = `<div style="color: #B5E1E0;">"scattered clouds" in the day.</div>`;
     } else if (NewIcon === "01d") {
       OldIcon.innerHTML = `<img src="images/01d.png" style="width: 300pt;"/>`;
       weatherIconSay.innerHTML = `<div style="color: #B5E1E0;">Its a "clear sky" in the day.</div>`;
@@ -179,7 +279,7 @@ function search(pop) {
       weatherIconSay.innerHTML = `<div style="color: #4C5588;">It has "few clouds" at night.</div>`;
     } else if (NewIcon === "03n") {
       OldIcon.innerHTML = `<img src="images/03n.png" style="width: 300pt;"/>`;
-      weatherIconSay.innerHTML = `<div style="color: #4C5588;">It has "scattered clouds" at night.</div>`;
+      weatherIconSay.innerHTML = `<div style="color: #4C5588;">"scattered clouds" at night.</div>`;
     } else if (NewIcon === "04n") {
       OldIcon.innerHTML = `<img src="images/04n.png" style="width: 300pt;"/>`;
       weatherIconSay.innerHTML = `<div style="color: #4C5588;">"broken clouds" at night.</div>`;
@@ -199,24 +299,24 @@ function search(pop) {
       OldIcon.innerHTML = `<img src="images/50n.png" style="width: 300pt;"/>`;
       weatherIconSay.innerHTML = `<div style="color: #4C5588;">Its "misty" at night.</div>`;
     }
+    getForecast(value.data.coord);
   }
   //END -- if statement icon //
-
   // --- END -- Function on Search field//
 
+  //--START- Let Variables/Call outs of Search Field//
   let SearchInput = document.querySelector("#SearchingInput");
   let SearchVal = SearchInput.value;
   let FirstLet = SearchVal.slice(0, 1);
   let CapFirstName = FirstLet.toUpperCase();
   let newCity = document.querySelector(".current-country");
-
   //-- START -- Weather API//
   let countryName = `${SearchVal}`;
   let unit = "metric";
   let addressUrl = `https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=dcd07d1aa5fa314f984ce1fb0e330deb&units=${unit}`;
   axios.get(addressUrl).then(realData);
   //-- END -- Weather API//
-
+  //START - Enter Nothing if statement//
   if (SearchVal === "") {
     alert(`Please Enter any Place :)`);
   } else {
@@ -226,15 +326,14 @@ function search(pop) {
     ).toLowerCase()},`;
   }
 }
-
+//END - Enter Nothing if statement//
 let form = document.querySelector("#cityForm");
 form.addEventListener("submit", search);
-
+//--END- Let Variables/Call outs of Search Field//
 //--START -- Current Button. detects your current location//
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(myPosition);
 }
-
 function myPosition(thisPosition) {
   let longitude = thisPosition.coords.longitude;
   let latitude = thisPosition.coords.latitude;
@@ -242,13 +341,13 @@ function myPosition(thisPosition) {
   let MyLocationApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=dcd07d1aa5fa314f984ce1fb0e330deb&units=${unit}`;
   axios.get(MyLocationApiUrl).then(MyNewDegrees);
 }
-
+//--END -- Current Button. detects your current location//
+//--START- Getting the My location Current Degrees//
 function MyNewDegrees(theDegrees) {
   let myTemperature = theDegrees.data.main.temp;
   let myHumidity = theDegrees.data.main.humidity;
   let myCityNewName = theDegrees.data.name;
   let myCountryNewName = theDegrees.data.sys.country;
-
   let newCountry = document.querySelector("#MyNewCountry");
   newCountry.innerHTML = ``;
   let humid = document.querySelector("#humid");
@@ -261,27 +360,26 @@ function MyNewDegrees(theDegrees) {
 
   newCity.innerHTML = `${myCountryNewName}, ${myCityNewName}`;
   let weatherIconSay = document.querySelector(".weatherIconSay");
-
-  ////////////////////////
+  //--END- Getting the My location Current Degrees//
+  //--START-- Cel & Far Functions//
   function Celcius() {
     let currentDegrees = document.querySelector(".current-degrees");
     currentDegrees.innerHTML = `${Math.ceil(myTemperature)} °C`;
   }
-
   function Far() {
     let currentDegrees = document.querySelector(".current-degrees");
     currentDegrees.innerHTML = `${Math.ceil((myTemperature * 9) / 5 + 32)} °F`;
   }
-
   let c = document.querySelector("#c");
   let f = document.querySelector("#f");
   c.addEventListener("click", Celcius);
   f.addEventListener("click", Far);
-  //////////////////////////
+  //--END-- Cel & Far Functions//
 
+  //--START-- My location Icons If else statement//
   if (NewIcon == "03d") {
     OldIcon.innerHTML = `<img src="images/03d.png" style="width: 300pt;"/>`;
-    weatherIconSay.innerHTML = `<div style="color: #B5E1E0;">It has "scattered clouds" in the day.</div>`;
+    weatherIconSay.innerHTML = `<div style="color: #B5E1E0;">"scattered clouds" in the day.</div>`;
   } else if (NewIcon === "01d") {
     OldIcon.innerHTML = `<img src="images/01d.png" style="width: 300pt;"/>`;
     weatherIconSay.innerHTML = `<div style="color: #B5E1E0;">Its a "clear sky" in the day.</div>`;
@@ -314,7 +412,7 @@ function MyNewDegrees(theDegrees) {
     weatherIconSay.innerHTML = `<div style="color: #4C5588;">It has "few clouds" at night.</div>`;
   } else if (NewIcon === "03n") {
     OldIcon.innerHTML = `<img src="images/03n.png" style="width: 300pt;"/>`;
-    weatherIconSay.innerHTML = `<div style="color: #4C5588;">It has "scattered clouds" at night.</div>`;
+    weatherIconSay.innerHTML = `<div style="color: #4C5588;">"scattered clouds" at night.</div>`;
   } else if (NewIcon === "04n") {
     OldIcon.innerHTML = `<img src="images/04n.png" style="width: 300pt;"/>`;
     weatherIconSay.innerHTML = `<div style="color: #4C5588;">"broken clouds" at night.</div>`;
@@ -335,6 +433,9 @@ function MyNewDegrees(theDegrees) {
     weatherIconSay.innerHTML = `<div style="color: #4C5588;">Its "misty" at night.</div>`;
   }
 }
+//--END- My location Icons If else statement//
 
-let currentButton = document.querySelector("#currentButton");
-currentButton.addEventListener("click", getCurrentPosition);
+// let currentButton = document.querySelector("#currentButton");
+// currentButton.addEventListener("click", getCurrentPosition);
+getCurrentPosition();
+displayForecast();
